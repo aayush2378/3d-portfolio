ef72f9545903e5cb38c80e656cbac420295c0e4d
aayush2378 <soni.aay@northeastern.edu>
2024-09-10 16:52:29 -0400

    Add fox and contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,137 @@
-import React from 'react'
+import React, { useState, useRef, Suspense } from 'react'
+import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+
+import Fox from '../models/Fox'
+import Loader from '../components/Loader'
 
 const Contact = () => {
+  const formRef = useRef(null);
+  const [form, setForm] = useState({name: '', email: '', message: ''});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name, 
+        to_name: "Aayush",
+        from_email: form.email,
+        to_email: 'aayushsoni237@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(()=> {
+      setIsLoading(false);
+
+      setForm({name: '', email: '', message: ''});
+    }).catch((error)=>{
+      setIsLoading(false);
+      console.error(error);
+    })
+  };
+
+  const handleBlur = () => {};
+  const handleFocus = () => {};
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch</h1>
+
+        <form className='w-full flex flex-col gap-7 mt-14' onSubmit={handleSubmit}>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input 
+            type='text'
+            name='name'
+            className='input'
+            placeholder='John'
+            required
+            value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            >
+            </input>
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input 
+            type='email'
+            name='email'
+            className='input'
+            placeholder='John.Doe@email.com'
+            required
+            value={form.email}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            >
+            </input>
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your message
+            <textarea 
+            name='message'
+            rows={4}
+            className='textarea'
+            placeholder='Let me know how I could help you!'
+            required
+            value={form.message}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            >
+            </textarea>
+          </label>
+          <button
+            type='submit'
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ?  'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+        camera={{
+          position: [0, 0, 5],
+          fov: 75,
+          near: 0.1, 
+          far: 1000 
+        }}
+        >
+        <directionalLight intensity={2.5} position={[0, 0, 1]}/>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+        <Suspense fallback={<Loader/>}>
+          <Fox
+            position = {[0.5, 0.35, 0]}
+            rotation = {[12.6, -0.6, 0]}
+            scale = {[0.5, 0.5, 0.5]}
+          />
+        </Suspense>
+
+        </Canvas>
+      </div>
+    </section>
   )
 }
 
