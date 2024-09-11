5b557962ffd4dc1eb78456c928a1749e986f914b
aayush2378 <soni.aay@northeastern.edu>
2024-09-11 18:05:48 -0400

    Complete project page

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className='CTA flex flex-col justify-center items-center py-16 px-6 text-center space-y-6'>
+      <p className='cta-text text-lg sm:text-2xl leading-relaxed'>
+        Have a project in mind? <br className='sm:block hidden'/>
+        Let's get started!
+      </p>
+      <Link to="/contact" className='btn px-8 py-4'>
+        Contact
+      </Link>
+    </section>
   )
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
         <h3 className='subhead-text'>Work Experience.</h3>
         <div className='mt-5 flex flex-col gap-3 text-slate-500'>
           <p>
-            I've worked with all sorts of companies, leveling up my skills and
+            I've worked with numerous technologies, leveling up my skills and
             teaming up with smart people. Here's the rundown:
           </p>
         </div>
@@ -58,15 +58,6 @@ const About = () => {
                 key={experience.company_name}
                 date={experience.date}
                 iconStyle={{ background: experience.iconBg }}
-                // icon={
-                //   <div className='flex justify-center items-center w-full h-full'>
-                //     <img
-                //       src={experience.icon}
-                //       alt={experience.company_name}
-                //       className='w-[60%] h-[60%] object-contain'
-                //     />
-                //   </div>
-                // }
                 contentStyle={{
                   borderBottom: "8px",
                   borderStyle: "solid",
